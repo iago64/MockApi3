00cf4ab2e49c3df55b4075ad7edb69a1d1c8de6d
Damian Facchini <dfacchini@exisoft.com.ar>
2023-01-23 15:24:31 -0300

    Improve Logs

--- MockApi3/Middlewares/RequestResponseLoggingMiddleware.cs
+++ MockApi3/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -31,12 +31,16 @@ namespace MockApi3.Middlewares
             context.Request.EnableBuffering();
             await using var requestStream = _recyclableMemoryStreamManager.GetStream();
             await context.Request.Body.CopyToAsync(requestStream);
-            _logger.LogInformation($"Http Request Information:{Environment.NewLine}" +
-                                   $"Schema:{context.Request.Scheme} " +
-                                   $"Host: {context.Request.Host} " +
-                                   $"Path: {context.Request.Path} " +
-                                   $"QueryString: {context.Request.QueryString} " +
-                                   $"Request Body: {ReadStreamInChunks(requestStream)}");
+            _logger.LogInformation($"***** Http Request Information");
+            _logger.LogInformation($"Schema:{context.Request.Scheme} ");
+            _logger.LogInformation($"Host: {context.Request.Host} ");
+            _logger.LogInformation($"Path: {context.Request.Path} ");
+            _logger.LogInformation($"QueryString: {context.Request.QueryString} ");
+            foreach (var header in context.Request.Headers)
+            {
+                _logger.LogInformation("Header: {Key}: {Value}", header.Key, header.Value);
+            }
+            _logger.LogInformation($"Request Body: {ReadStreamInChunks(requestStream)}");
             context.Request.Body.Position = 0;
         }
         private static string ReadStreamInChunks(Stream stream)
@@ -64,12 +68,16 @@ namespace MockApi3.Middlewares
             context.Response.Body.Seek(0, SeekOrigin.Begin);
             var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
             context.Response.Body.Seek(0, SeekOrigin.Begin);
-            _logger.LogInformation($"Http Response Information:{Environment.NewLine}" +
-                                   $"Schema:{context.Request.Scheme} " +
-                                   $"Host: {context.Request.Host} " +
-                                   $"Path: {context.Request.Path} " +
-                                   $"QueryString: {context.Request.QueryString} " +
-                                   $"Response Body: {text}");
+            _logger.LogInformation($"***** Http Response Information");
+            _logger.LogInformation($"Schema:{context.Request.Scheme} ");
+            _logger.LogInformation($"Host: {context.Request.Host} ");
+            _logger.LogInformation($"Path: {context.Request.Path} ");
+            _logger.LogInformation($"QueryString: {context.Request.QueryString} ");
+            foreach (var header in context.Request.Headers)
+            {
+                _logger.LogInformation("Header: {Key}: {Value}", header.Key, header.Value);
+            }
+            _logger.LogInformation($"Response Body: {text}");
             await responseBody.CopyToAsync(originalBodyStream);
         }
     }
